0fbb6d9c7d1e09c37555d661144895c869dfc7ef
Remy Moll <36770313+L0rd0fB0red0m@users.noreply.github.com>
2019-01-31 14:08:36 +0100

    Update lp_final.py

--- lp_final.py
+++ lp_final.py
@@ -111,7 +111,7 @@ for entry in list_already_parsed:
          
 for old_img in image_old_list:
     if (old_img[:old_img.index(".ppm")] + ".json") not in image_already_parsed:
-        read_image("images/"+_,intermed_counter)
+        read_image("images/"+old_img,intermed_counter)
         intermed_counter+=1
 
 clear_strip()
